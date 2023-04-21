e93a7623b04c94084cdc98df7ab5df4dd3c1f13a
greyson4 <129144827+greyson4@users.noreply.github.com>
2023-04-21 15:24:27 +0800

    Update index.tsx

--- src/modules/Sidebar/index.tsx
+++ src/modules/Sidebar/index.tsx
@@ -70,13 +70,6 @@ const Sidebar: FC<{
     <div className="flex flex-col h-full bg-white">
       <div className="p-4 mt-2 flex items-baseline justify-between">
         <span className="text-3xl text-gradient font-[800]">AI聊天独享版</span>
-        <a
-          href="https://github.com/GPTGenius/chatgpt-vercel"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <i className="ml-2 ri-github-fill text-xl" />
-        </a>
       </div>
       <div className="p-2 flex items-center justify-between mb-4">
         <div className="rounded-xl h-10 border flex-1">
